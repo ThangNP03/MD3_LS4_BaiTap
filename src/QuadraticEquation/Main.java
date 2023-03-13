8105f7976b719fba058cc03a734516674e59d066
ThangNP03 <2003nguyenphucthang@gmail.com>
2023-03-13 11:08:04 +0700

    MD3_LS4_BaiTap

---
+++ src/QuadraticEquation/Main.java
@@ -0,0 +1,8 @@
+package QuadraticEquation;
+
+public class Main {
+    public static void main(String[] args) {
+        QuadraticEquation pt1 = new QuadraticEquation(1,3,1);
+        pt1.getRoot();
+    }
+}
